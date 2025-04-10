fbfa8c12b444fc919cf3483e70c9c0658a207dcb
Vinicius Silva Cruz <cruztheripper@gmail.com>
2025-04-10 19:01:07 -0300

    Java SpringBoot, o inimigo agora é outro

---
+++ src/main/java/com/perritotoystore/PerritotoystoreApplication.java
@@ -0,0 +1,13 @@
+package com.perritotoystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerritotoystoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerritotoystoreApplication.class, args);
+	}
+
+}
