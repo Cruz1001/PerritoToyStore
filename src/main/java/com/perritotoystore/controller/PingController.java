022140b63ffd82bf1f5784ec4a9a560006688188
sabrainas <sazonass.m@gmail.com>
2025-04-10 22:06:13 -0300

    configurando o swagger e documentando os endpoints

--- src/main/java/com/perritotoystore/controller/PingController.java
+++ src/main/java/com/perritotoystore/controller/PingController.java
@@ -1,9 +1,11 @@
 package com.perritotoystore.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Ping")
 public class PingController {
 
     @GetMapping("/api/ping")
