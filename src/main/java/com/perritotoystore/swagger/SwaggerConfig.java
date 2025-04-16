a3d78b3fda5bcd57d33b00ee7d102fe2dd7fd7a4
Vinicius Silva Cruz <viniokcruz@gmail.com>
2025-04-16 03:03:23 -0300

    Adaptando o Swagger para fazer um post no BD

--- src/main/java/com/perritotoystore/swagger/SwaggerConfig.java
+++ src/main/java/com/perritotoystore/swagger/SwaggerConfig.java
@@ -3,6 +3,9 @@ package com.perritotoystore.swagger;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.ObjectSchema;
+import io.swagger.v3.oas.models.media.StringSchema;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,6 +21,16 @@ public class SwaggerConfig {
                         .description("API para gerenciamento da Perrito Toy Store")
                         .contact(new Contact()
                                 .name("Perrito")
+                        ))
+                        .components(new io.swagger.v3.oas.models.Components()
+                        .addSchemas("Brinquedo", new ObjectSchema()
+                                .addProperties("codigo", new StringSchema())
+                                .addProperties("descricao", new StringSchema())
+                                .addProperties("categoria", new StringSchema())
+                                .addProperties("marca", new StringSchema())
+                                .addProperties("img", new StringSchema())
+                                .addProperties("valor", new StringSchema())
+                                .addProperties("detalhes", new StringSchema())
                         ));
     }
 }
